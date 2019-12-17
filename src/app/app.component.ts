05f6f90e7612998a9942e7da7d5d789a35f32959
santosh <santosh.ece06@gmail.com>
2019-12-17 10:54:19 +0530

    img.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gridemo';
+  public list = [];
+
+  public ngOnInit() {
+    this.list = [
+      { rows: 2, cols: 2, data: 1 },
+      { rows: 1, cols: 1, data: 2 },
+      { rows: 2, cols: 1, data: 3 },
+      { rows: 1, cols: 1, data: 4 },
+
+      { rows: 1, cols: 1, data: 5 },
+      { rows: 2, cols: 2, data: 6 },
+      { rows: 1, cols: 1, data: 7 },
+      { rows: 1, cols: 1, data: 8 },
+      { rows: 1, cols: 1, data: 9 },
+
+      { rows: 1, cols: 1, data: 10 },
+      { rows: 1, cols: 1, data: 11 },
+      { rows: 2, cols: 2, data: 12 },
+      { rows: 1, cols: 1, data: 13 },
+      { rows: 1, cols: 1, data: 14 },
+      
+    ];
+  }
 }
